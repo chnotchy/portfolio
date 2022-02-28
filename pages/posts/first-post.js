59419dbedeb632af16f2738f6fcfd0d721a3bf55
notchcoder <kokiuaxdx@gmail.com>
2022-03-01 00:35:33 +0900

    add css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,21 @@
 import Head from "next/head"
 import Link from "next/link"
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
   const a = "First Post"
   return (
     <div>
-      <Head>
-        <title>Next App First Post</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>{a}</h1>
-      <Link href="/">
-        <a>HOME</a>
-      </Link>
+      <Layout>
+        <Head>
+          <title>Next App First Post</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1>{a}</h1>
+        <Link href="/">
+          <a>HOME</a>
+        </Link>
+      </Layout>
     </div>
   )
 }
