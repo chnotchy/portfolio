118b19b9f0bd6b4cd4ece2353abbc82249b99a39
notchcoder <kokiuaxdx@gmail.com>
2022-03-05 01:31:14 +0900

    fix grid

--- pages/card.js
+++ pages/card.js
@@ -11,35 +11,6 @@ export default function Card() {
       <section>
         <div className='cardSection'>
           <h2 className='title'>
-            Flex
-          </h2>
-          <div className='cardContainerF'>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='cardStyle cardF'>
-              <div className='cardContent'>カード</div>
-            </div>
-            <div className='adjustLeftF'></div>
-          </div>
-        </div>
-        <div className='cardSection'>
-          <h2 className='title'>
             Grid
           </h2>
           <div className='cardContainerG'>
@@ -64,31 +35,34 @@ export default function Card() {
             <div className='cardStyle cardG'>
               <div className='cardContent'></div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+          </div>
+        </div>
+        <div className='cardSection'>
+          <h2 className='title'>
+            Flex
+          </h2>
+          <div className='cardContainerF'>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
+            <div className='cardStyle cardF'>
+              <div className='cardContent'>カード</div>
             </div>
-            <div className='cardStyle cardG'>
-              <div className='cardContent'></div>
-            </div>
-            <div className='adjustLeft'></div>
           </div>
         </div>
         <style jsx>{`
@@ -123,6 +97,17 @@ export default function Card() {
             width: 100%;
             left: 0;
           }
+          .cardContainerG {
+            display: grid;
+            width: 95%;
+            margin: auto;
+            column-gap: 32px;
+            row-gap: 32px;
+            grid-auto-rows: minmax(160px, auto);
+            grid-template-columns: repeat(auto-fill, max(20%, calc(600px - 50vw)));
+            justify-content: center;
+            align-content: center;
+          }
           .cardContainerF {
             display: flex;
             flex-wrap: wrap;
@@ -135,41 +120,6 @@ export default function Card() {
             display: flex;
             margin: 16px;
             height: 160px;
-          }
-          .adjustLeftF {
-            display: none;
-            /* visibility: hidden; */
-            /* flex-grow: 1; */
-            /* flex-basis: 100%; */
-          }
-          .cardContainerGa {
-            display: grid;
-            width: 95%;
-            margin: auto;
-            /* column-gap: clamp(16px, 10vw, 100px); */
-            column-gap: 0;
-            justify-content: space-around;
-            grid-template-columns: repeat(auto-fill, 8px 30% 8px);
-            grid-auto-rows: minmax(160px, auto);
-            row-gap: clamp(16px, 5vw, 32px);
-            align-content: space-evenly;
-          }
-          .cardContainerG {
-            display: grid;
-            width: 95%;
-            margin: auto;
-            column-gap: clamp(16px, 10vw, 100px);
-            row-gap: clamp(16px, 5vw, 32px);
-            /* grid-template-rows: auto; */
-            grid-auto-rows: minmax(160px, auto);
-            /* grid-template-columns: repeat(4, minmax(160px, 300px)); */
-            grid-template-columns: repeat(auto-fill, clamp(200px, 20%, 400px));
-            justify-content: space-evenly;
-            align-content: space-evenly;
-          }
-          .cardG {
-            color: white;
-            /* margin: 16px; */
           }
         `}</style>
       </section>
