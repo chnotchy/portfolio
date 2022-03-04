3317b89bbdbbb0beadd73a6a1b37951bea4c65df
notchcoder <kokiuaxdx@gmail.com>
2022-03-04 23:42:19 +0900

    add grid

--- pages/card.js
+++ pages/card.js
@@ -43,26 +43,50 @@ export default function Card() {
             Grid
           </h2>
           <div className='cardContainerG'>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
-            <div className='cardStyle'>
-              <div className='cardContent'>カード</div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
+            </div>
+            <div className='cardStyle cardG'>
+              <div className='cardContent'></div>
             </div>
             <div className='adjustLeft'></div>
           </div>
@@ -75,9 +99,7 @@ export default function Card() {
           .cardStyle {
             text-align: center;
             padding: 20px;
-            margin: 16px;
             border-radius: 4px;
-            height: 100px;
             background: #555;
             transition: .3s;
             position: relative;
@@ -111,6 +133,8 @@ export default function Card() {
             flex-basis: 300px;
             flex-grow: 1;
             display: flex;
+            margin: 16px;
+            height: 160px;
           }
           .adjustLeftF {
             display: none;
@@ -118,10 +142,34 @@ export default function Card() {
             /* flex-grow: 1; */
             /* flex-basis: 100%; */
           }
+          .cardContainerGa {
+            display: grid;
+            width: 95%;
+            margin: auto;
+            /* column-gap: clamp(16px, 10vw, 100px); */
+            column-gap: 0;
+            justify-content: space-around;
+            grid-template-columns: repeat(auto-fill, 8px 30% 8px);
+            grid-auto-rows: minmax(160px, auto);
+            row-gap: clamp(16px, 5vw, 32px);
+            align-content: space-evenly;
+          }
           .cardContainerG {
             display: grid;
-            grid-template-rows: 1fr;
-            grid-template-columns: 1fr 1fr;
+            width: 95%;
+            margin: auto;
+            column-gap: clamp(16px, 10vw, 100px);
+            row-gap: clamp(16px, 5vw, 32px);
+            /* grid-template-rows: auto; */
+            grid-auto-rows: minmax(160px, auto);
+            /* grid-template-columns: repeat(4, minmax(160px, 300px)); */
+            grid-template-columns: repeat(auto-fill, clamp(200px, 20%, 400px));
+            justify-content: space-evenly;
+            align-content: space-evenly;
+          }
+          .cardG {
+            color: white;
+            /* margin: 16px; */
           }
         `}</style>
       </section>
